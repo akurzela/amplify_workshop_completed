c1923d41fe7509b76838f093e0f2bae59af3044a
Adrianna Kurzela <akurzela@amazon.lu>
2022-02-17 15:20:36 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
+import BasicTable from './components/Table';
 
-function App() {
-  return (
+Amplify.configure(awsconfig);
+
+const AuthStateApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData)
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "20px", marginLeft: "80%" }} ><AmplifySignOut /></div>
+      <div>Hello, {user.username}</div>
+      <div style={{ width: "80%", margin: "0 auto" }} ><BasicTable /></div>
     </div>
+  ) : (
+    <AmplifyAuthenticator />
   );
 }
 
-export default App;
+export default AuthStateApp;
